9ac3f98050baa5ddc885b27cdbd42bee5bbdc399
Your GitHub Username <101971819+dhereprathamesh@users.noreply.github.com>
2024-06-22 17:19:36 +0530

    data Types

---
+++ cssBasic/dataTypes.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //int
+    int x = 10;
+
+    //long
+    long y = 5;
+
+    //long long
+    long long z = 150000000;
+
+    //float 
+    float a = 5.4;
+    
+    //double
+    double b = 6;
+
+    cout << "Value is: " << x << " " << y << " " << z << " " << a << " " << b << endl; 
+
+    //String
+    string s1;
+    string s2;
+    cin >> s1 >> s2;
+    cout << s1 << " " << s2 << endl;
+
+
+
+    //getline
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+
+    return 0; 
+}
